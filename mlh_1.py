2d4c8fecbf7603d6e895c2869a35a2b570553bb6
bindaldhara <84957152+bindaldhara@users.noreply.github.com>
2021-06-29 09:41:14 +0530

    Add files via upload

---
+++ mlh_1.py
@@ -0,0 +1,11 @@
+numbers = [1, 3, 4, 2]
+  
+# Sorting list of Integers in ascending
+numbers.sort()
+  
+print(numbers)
+  
+# Sorting list of Integers in descending
+numbers.sort(reverse = True)
+  
+print(numbers)
